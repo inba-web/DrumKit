fc49e1a0f5b0f07c9890fde0dbe8b372a58679ec
Inba <inbafreakz@gmail.com>
2025-03-28 10:30:47 +0530

    Initial commit - Drum Kit Project

---
+++ index.js
@@ -0,0 +1,61 @@
+for(var i = 0 ; i<document.querySelectorAll(".drum").length ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var button = this.innerHTML;
+        makesound(button);
+        button_Animation(button);
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    button_Animation(event.key);
+});
+
+
+function makesound(key){
+    switch(key){
+        case "w":
+            var w = new Audio("./sounds/tom-1.mp3");
+            w.play();
+            break;
+
+        case "a":
+            var a = new Audio("./sounds/tom-2.mp3");
+            a.play();
+            break;
+
+        case "s":
+            var s = new Audio("./sounds/tom-3.mp3");
+            s.play();
+            break;
+
+        case "d":
+            var d = new Audio("./sounds/tom-4.mp3");
+            d.play();
+            break;
+
+        case "j":
+            var j = new Audio("./sounds/snare.mp3");
+            j.play();
+            break;
+
+        case "k":
+            var k = new Audio("./sounds/crash.mp3");
+            k.play();
+            break;
+
+        case "l":
+            var l = new Audio("./sounds/kick-bass.mp3");
+            l.play();
+            break;
+    }
+}
+
+function button_Animation(currentKey){
+    var activeButton =  document.querySelector("."+ currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+};
